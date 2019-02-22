0646a7a82006ad52cbc22f7d07baf240cd28528c
Suzanne Enfield <cutopia@Suzannes-iMac.local>
2019-02-21 23:46:50 -0800

    initial commit of project still in progress

---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour {
+    [SerializeField] public int width = 16;
+    [SerializeField] public int height = 12;
+    [SerializeField] public GameObject mapTilePrefab;
+    [SerializeField] public Color wallColor;
+    [SerializeField] public Color pathColor;
+    [SerializeField] public int smoothIterations = 5;
+    GameObject[,] gameMap;
+    
+	// Use this for initialization
+	void Start () {
+        InitializeGameMap();
+        LinkMap();
+        GenerateMap(1, 1);
+        SmoothMap(smoothIterations);
+	}
+    
+    /// <summary>
+    /// Link the map units together so they know their neighbors for quick calculating.
+    /// </summary>
+    void LinkMap()
+    {
+        for (int col = 0; col < width; col++)
+        {
+            for (int row = 0; row < height; row++)
+            {
+                var tileInfo = gameMap[col, row].GetComponent<MapUnit>();
+                if (row - 1 >= 0)
+                {
+                    tileInfo.neighbors[0] = gameMap[col, row - 1];
+                }
+                if (row + 1 < height)
+                {
+                    tileInfo.neighbors[1] = gameMap[col, row + 1];
+                }
+                if (col - 1 >= 0)
+                {
+                    tileInfo.neighbors[2] = gameMap[col - 1, row];
+                }
+                if (col + 1 < width)
+                {
+                    tileInfo.neighbors[3] = gameMap[col + 1, row];
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Initializes the game map.
+    /// </summary>
+    void InitializeGameMap()
+    {
+        gameMap = new GameObject[width, height];
+        for (int col = 0; col < width; col++)
+        {
+            for (int row = 0; row < height; row++)
+            {
+                GameObject tile = Instantiate(mapTilePrefab) as GameObject;
+                tile.transform.position = new Vector3(col, row, 0);
+                tile.GetComponent<SpriteRenderer>().color = wallColor;
+                var tileInfo = tile.GetComponent<MapUnit>();
+                // helpful for figuring out who got clicked later.
+                tileInfo.col = col;
+                tileInfo.row = row;
+                tile.transform.SetParent(transform);
+                gameMap[col, row] = tile;
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Run a smoothing passes on the map after it is generated to 
+    /// give it an airier feel.
+    /// </summary>
+    void SmoothMap(int iterations)
+    {
+        for (int i = 0; i < iterations; i++)
+        {
+            for (int col = 0; col < width; col++)
+            {
+                for (int row = 0; row < height; row++)
+                {
+                    if (gameMap[col, row].GetComponent<MapUnit>().wall)
+                    {
+                        if (GetWallNeighbors(col, row) == 1)
+                        {
+                            gameMap[col, row].GetComponent<MapUnit>().wall = false;
+                            gameMap[col, row].GetComponent<SpriteRenderer>().color = pathColor;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Count number of neighbors that are walls. This version does not count where the map meets the outside world as a wall.
+    /// </summary>
+    /// <returns>The wall neighbors.</returns>
+    /// <param name="locCol">Location col.</param>
+    /// <param name="locRow">Location row.</param>
+    int GetWallNeighbors(int locCol, int locRow)
+    {
+        int total = 0;
+        for (int col = (locCol - 1 >= 0) ? locCol - 1 : 0; (col < locCol + 2) && (col < width); col++)
+        {
+            for (int row = (locRow - 1 >= 0) ? locRow - 1 : 0; (row < locRow + 2) && (row < height); row++)
+            {
+                if (row == locRow && col == locCol)
+                {
+                    continue;
+                }
+                if (gameMap[col, row].GetComponent<MapUnit>().wall)
+                {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+
+    /// <summary>
+    /// Let's carve out a random maze map for the demo.
+    /// </summary>
+    /// <param name="col">Col.</param>
+    /// <param name="row">Row.</param>
+    void GenerateMap(int col, int row)
+    {
+        var tileInfo = gameMap[col, row].GetComponent<MapUnit>();
+        gameMap[col, row].GetComponent<SpriteRenderer>().color = pathColor;
+        tileInfo.visited = true;
+        tileInfo.wall = false;
+        
+        List<int[]> validDirs = GetValidDirs(col, row);
+        while (validDirs.Count > 0)
+        {
+            int[] chosenDir = validDirs[Random.Range(0, validDirs.Count)];
+            int nextCol = (int)((chosenDir[0] + col) * 0.5);
+            int nextRow = (int)((chosenDir[1] + row) * 0.5);
+            gameMap[nextCol, nextRow].GetComponent<MapUnit>().wall = false;
+            gameMap[nextCol, nextRow].GetComponent<SpriteRenderer>().color = pathColor;
+            GenerateMap(chosenDir[0], chosenDir[1]);
+            validDirs = GetValidDirs(col, row);
+        }
+    }
+
+    /// <summary>
+    /// find current valid dirs for maze generation.
+    /// </summary>
+    /// <returns>The valid dirs.</returns>
+    /// <param name="col">Col.</param>
+    /// <param name="row">Row.</param>
+    List<int[]> GetValidDirs(int col, int row)
+    {
+        List<int[]> validDirs = new List<int[]>();
+        if ((col - 2 >= 0) && !gameMap[col - 2, row].GetComponent<MapUnit>().visited)
+        {
+            validDirs.Add(new int[] { col - 2, row });
+        }
+        if ((col + 2 < width) && !gameMap[col + 2, row].GetComponent<MapUnit>().visited)
+        {
+            validDirs.Add(new int[] { col + 2, row });
+        }
+        if ((row - 2 >= 0) && !gameMap[col, row - 2].GetComponent<MapUnit>().visited)
+        {
+            validDirs.Add(new int[] { col, row - 2 });
+        }
+        if ((row + 2 < height) && !gameMap[col, row + 2].GetComponent<MapUnit>().visited)
+        {
+            validDirs.Add(new int[] { col, row + 2 });
+        }
+        return validDirs;
+    }
+}
---
+++ Assets/Scripts/MapUnit.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MapUnit : MonoBehaviour
+{
+    [SerializeField] public SpriteRenderer spriteHighlight;
+    [SerializeField] public Color startHighlightColor;
+    [SerializeField] public Color destinationHighlightColor;
+    private const int INFINITY_DISTANCE = 100;
+    public GameObject[] neighbors = new GameObject[4];
+    public bool visited = false;
+    public bool wall = true;
+    public int col;
+    public int row;
+    public int startingMoves = 7;
+    public int currentDistance = INFINITY_DISTANCE;
+
+    public void OnMouseDown()
+    {
+        Propagate(startingMoves);
+    }
+
+    public void OnMouseOver()
+    {
+        Debug.Log("OnMouseOVer");
+        if (currentDistance == INFINITY_DISTANCE)
+        {
+            spriteHighlight.color = startHighlightColor;
+        }
+        else
+        {
+            spriteHighlight.color = destinationHighlightColor;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        spriteHighlight.color = Color.clear;
+    }
+
+    public void Propagate(int movesLeft)
+    {
+        if (wall == true)
+        {
+            return;
+        }
+        if (currentDistance < (startingMoves - movesLeft))
+        {
+            return;
+        }
+        if (movesLeft > 0)
+        {
+            currentDistance = startingMoves - movesLeft;
+            gameObject.GetComponentInChildren<TextMesh>().text = " " + currentDistance.ToString();
+            for (int i = 0; i < neighbors.Length; i++)
+            {
+                if (neighbors[i] != null)
+                {
+                    neighbors[i].GetComponent<MapUnit>().Propagate(movesLeft - 1);
+                }
+            }
+        }
+    }
+}
